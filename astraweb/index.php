f8923de120b8ccc6cc4e9ee289c01f533352a1e0
localcodeHost <129775833+localcodeHost@users.noreply.github.com>
2023-04-08 10:07:10 +0530

    Add files via upload

---
+++ astraweb/index.php
@@ -0,0 +1,561 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/banner.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    />
+    <link rel="stylesheet" href="css/feature.css">
+    <link rel="stylesheet" href="css/card-p-deatil.css">
+    <link rel="stylesheet" href="css/benifit.css">
+    <link rel="stylesheet" href="css/work.css">
+    <link rel="stylesheet" href="css/road-banner.css">
+    <link rel="stylesheet" href="css/post.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <title>my web</title>
+  </head>
+  <body>
+    <div class="root">
+      <header>
+        <nav class="navbar">
+          <div class="logo">astra</div>
+          <div class="right-menu nav-toogle-bar ">
+            <ul class="navbar-ul">
+              <li><a href="">home</a></li>
+              <li><a href="">about</a></li>
+              <li><a href="">services</a></li>
+              <li><a href="">content</a></li>
+              <li>
+                <a href=""> <i class="fa-brands fa-instagram"></i></a>
+              </li>
+              <li>
+                <a href=""> <i class="fa-brands fa-facebook"></i></a>
+              </li>
+              <li>
+                <a href=""> <i class="fa-brands fa-twitter"></i></a>
+              </li>
+            </ul>
+          </div>
+          <div class="menu-bar">
+            <div class="burger"></div>
+            <div class="burger"></div>
+            <div class="burger"></div>
+          </div>
+        </nav>
+      </header>
+      <div class="root-wraper">
+        <div class="text-root">
+            <h2>your idea matters</h2>
+            <p>pulvinar enim ac tortor nulla facilisi tristique facilisi elementum solocitudin eget lorem</p>
+            <div class="root-btn-text">
+                <button class="btn-taxt">
+                    make a website
+                </button>
+            </div>
+        </div>
+        <div class="img-root">
+            <img src="img/cartoon.png" alt="root-cartoon" width="300px" class="cartoon-img">
+        </div>
+      </div>
+    </div>
+    <div class="feature-card-wrap">
+<div class="wrap-f-card">
+    <div class="icon-box">
+        <i class="fa-solid fa-building"></i>
+    </div>
+    <div class="text-box-f-card">
+        <h3 class="card-f-heading">
+            local business
+    
+        </h3>
+        <p class="card-t-f-p">
+            lorem ipsum dolor consectetur 
+            adipiscing edit eiusmod.
+        </p>
+    </div>
+</div>
+<div class="wrap-f-card">
+    <div class="icon-box">
+        <i class="fa-solid fa-bag-shopping"></i>
+    </div>
+    <div class="text-box-f-card">
+        <h3 class="card-f-heading">
+            online store
+    
+        </h3>
+        <p class="card-t-f-p">
+            lorem ipsum dolor consectetur 
+            adipiscing edit eiusmod.
+        </p>
+    </div>
+</div><div class="wrap-f-card">
+    <div class="icon-box">
+        <i class="fa-solid fa-pen-to-square"></i>
+
+    </div>
+    <div class="text-box-f-card">
+        <h3 class="card-f-heading">
+           blogging
+    
+        </h3>
+        <p class="card-t-f-p">
+            lorem ipsum dolor consectetur 
+            adipiscing edit eiusmod.
+        </p>
+    </div>
+</div><div class="wrap-f-card">
+    <div class="icon-box">
+ 
+<i class="fa-solid fa-check"></i>
+    </div>
+    <div class="text-box-f-card">
+        <h3 class="card-f-heading">
+          portfolio
+    
+        </h3>
+        <p class="card-t-f-p">
+            lorem ipsum dolor consectetur 
+            adipiscing edit eiusmod.
+        </p>
+    </div>
+</div>
+
+    </div>
+    <div class="pro-card-details">
+      <div class="p-f-t">
+        <p class="p-f-pera">
+          2,369,964 + <span class="p-f-pera-span">websites trust astra and so should you!</span>
+        </p>
+      </div>
+      <div class="p-f-c-card-ing">
+        <img class="details-pro-card-img" src="img/bob-jones-uni.png" alt="bob">
+        <img class="details-pro-card-img" src="img/stf.png" alt="stf">
+     
+       
+        <img class="details-pro-card-img" src="img/nasa.png" alt="nase">
+        <img class="details-pro-card-img" src="img/logo-google.png" alt="google">
+      
+        <img class="details-pro-card-img" src="img/liquid-web.png" alt="liq">
+
+      </div>
+    </div>
+    <div class="review-card-wraper">
+      <div class="top-message-re-details">
+        <h3 class="re-p">
+          5 Stars Based on 5,678+ Real Users Reviews
+        
+        </h3>
+      </div>
+      <div class="review-card">
+<div class="re-card">
+  <img src="img/wp-logo.webp" alt="wp-logo">
+  <p class="re-rating">
+    5083+ Ratings
+  </p>
+  <div class="rating-star">
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+  </div>
+
+</div>
+<div class="re-card">
+  <img src="img/trust-pilot.webp" alt="wp-logo">
+  <p class="re-rating">
+    5083+ Ratings
+  </p>
+  <div class="rating-star">
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+    <i class="fa-sharp fa-solid fa-star"></i>
+  </div>
+
+</div>
+<div class="man-card">
+  <div class="man-info">
+    <img src="img/man.webp" class="man-img" alt="man">
+    <p class="name-m">
+      Chris Lema
+    </p>
+   
+  </div>
+  <div class="man-p">
+    <p class="man-p-i">Quotes icon
+      At Liquid Web we tested over 50 themes for WooCommerce – looking at both performance and price.
+    <span class="man-p-span"> 
+      Astra cleanly beat the competition. It’s why we recommend it to all our customers.
+    </span>
+    </p>
+  </div>
+</div>
+      </div>
+    </div>
+    <div class="befit-wrap">
+      <div class="wrap-b-container">
+        <div class="b-card">
+          <div class="b-icon">
+            <i class="fa-solid fa-arrow-up-from-bracket"></i>
+          </div>
+          <div class="b-text">
+            <h4 class="b-heading">
+              Pre-Built Websites
+            </h4>
+            <p class="b-pera">
+              Reduce website design time by using pixel perfect ready to use website demos from our library of starter templates.
+            </p>
+          </div>
+        </div>
+        <div class="b-card">
+          <div class="b-icon">
+            <i class="fa-solid fa-code"></i>
+          </div>
+          <div class="b-text">
+            <h4 class="b-heading">
+              Customize Without Code​
+            </h4>
+            <p class="b-pera">
+              Reduce website design time by using pixel perfect ready to use website demos from our library of starter templates.
+            </p>
+          </div>
+        </div>
+        <div class="b-card">
+          <div class="b-icon">
+            <i  class="fa-solid fa-house"></i>
+          </div>
+          <div class="b-text">
+            <h4 class="b-heading">
+              Made for Page Builders
+            </h4>
+            <p class="b-pera">
+              Reduce website design time by using pixel perfect ready to use website demos from our library of starter templates.
+            </p>
+          </div>
+        </div>
+        <div class="b-card">
+          <div class="b-icon">
+            <i class="fa-solid fa-rocket"></i>
+          </div>
+          <div class="b-text">
+            <h4 class="b-heading">
+              Super Fast Performance
+            </h4>
+            <p class="b-pera">
+              Reduce website design time by using pixel perfect ready to use website demos from our library of starter templates.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="show-word-container">
+      <div class="top-show-contaoner">
+        <h2 class="top-show-w-h">
+          Customize Everything Easily
+        </h2>
+        <p class="show-w-p">
+          Not only will your website be fast, but you will be able to  make it look  exactly how <br> you want with our visual theme customizer, fast & easy!
+        </p>
+        </div>
+
+      
+      <div class="gird-container">
+        <div class="gird-wrap-1">
+          <div class="clihs-grid-1">
+            <h3 class="g-heading">
+              Layout Settings
+            </h3>
+            <p class="g-p">
+              In this area, one can manage layout of the website container, header, blog, archives, single pages, posts, sidebar & footer.
+            </p>
+          </div>
+          <div class="clihs-grid-2">
+            <h3 class="g-heading">
+              Header Options
+            </h3>
+            <p class="g-p">
+              Astra comes with several header layouts built-in. The Pro addon extends the possibilities and adds more features further.
+            </p>
+          </div>
+        </div>
+        <div class="gird-wrap-2">
+          <img src="img/pic-back.webp"class="pic-img" alt="pic-art">
+        </div>
+        <div class="gird-wrap-1">
+          <div class="clihs-grid-1">
+            <h3 class="g-heading">
+              
+Colors & Typography
+            </h3>
+            <p class="g-p">
+              Set colors & fonts easily! We understand it's important to be able to set fonts & colors that integrate with your brand.
+            </p>
+          </div>
+          <div class="clihs-grid-2">
+            <h3 class="g-heading">
+              
+              Blog & Archives
+
+                          </h3>
+                          <p class="g-p">
+                            Control design of your blog posts & archive pages with neat options in the customizer. Manage width, content and even meta.
+                          </p>
+          </div>
+        </div>
+      </div>
+    
+
+    </div>
+    <div class="company-tag">
+      <p class="tag-p">
+        Full & Seamless Integration With Your Favorite Tools
+      </p>
+      <div class="com-tag">
+        <img src="img/part-01.webp" alt="">
+        <img src="img/part-2.jpg" alt="">
+        <img src="img/part3.webp" alt="">
+      </div>
+    </div>
+    <div class="info-container">
+      <div class="wrap-info-con">
+        <div class="img-info">
+          <img src="img/gutenberg-astra.webp"  alt="">
+        </div>
+        <div class="tag-info">
+          <img src="img/icon-128x128-1-1.jpg" alt="">
+          <h3 class="tag-info-heading">
+            Gutenberg Blocks -
+Seamless Compatibility
+          </h3>
+          <p class="tag-info-p">
+            Even before the official release of Gutenberg, Astra was among the first themes to become Gutenberg compatible. Over the years, this compatibility grew stronger. Today, Astra offers the most extensive library of free starter templates for Gutenberg, the ability to create custom layouts with the editor, and automatically manages Gutenberg blocks’ styling and spacing.
+          </p>
+        </div>
+      </div>
+    </div>
+<div class="road-banner">
+<div class="road-banner-heading">
+  <h3 class="r-heading">
+    Unparalleled Performance
+  </h3>
+  <p class="r-p">
+    Every single line of code in Astra is profiled for fast performing websites. We have numbers to prove that.
+  </p>
+</div>
+<div class="road-banner-cards">
+  <div class="r-b-card-wrap">
+    <span class="r-b-card-span-icon-text">
+      <h4 class="rb-card-h4">
+        <i class="fa-solid fa-minus"></i>  No jQuery
+      </h4>
+    </span>
+    <div class="rb-card-wrap-p">
+      Many times, render blocking jQuery comes in the way of performance optimization. So Astra uses Vanilla JavaScript instead.
+    </div>
+  </div>
+  <div class="r-b-card-wrap">
+    <span class="r-b-card-span-icon-text">
+      <h4 class="rb-card-h4">
+        <i class="fa-solid fa-minus"></i>  No jQuery
+      </h4>
+    </span>
+    <div class="rb-card-wrap-p">
+      Many times, render blocking jQuery comes in the way of performance optimization. So Astra uses Vanilla JavaScript instead.
+    </div>
+  </div>
+  <div class="r-b-card-wrap">
+    <span class="r-b-card-span-icon-text">
+      <h4 class="rb-card-h4">
+        <i class="fa-solid fa-minus"></i>  No jQuery
+      </h4>
+    </span>
+    <div class="rb-card-wrap-p">
+      Many times, render blocking jQuery comes in the way of performance optimization. So Astra uses Vanilla JavaScript instead.
+    </div>
+  </div>
+  <div class="absolute-card">
+    <div class="absolute-c-wrap">
+      <img src="img/pingodom.jpg" alt="pingodom">
+      <p class="percentage-ab-card">
+        100%
+      </p>
+      <p class="d-ab-p">
+        Speed Grade
+      </p>
+    </div>
+    <div class="absolute-c-wrap">
+      <img src="img/google.jpg" alt="google">
+      <p class="percentage-ab-card">
+        95%
+      </p>
+      <p class="d-ab-p">
+        Speed Grade
+      </p>
+    </div>
+    <div class="absolute-c-wrap">
+      <img src="img/gt.jpg" alt="pingodom">
+      <p class="percentage-ab-card">
+        700ms
+      </p>
+      <p class="d-ab-p">
+        Speed Grade
+      </p>
+    </div>
+    <div class="absolute-c-wrap">
+      <img src="img/y-slow.jpg" alt="pingodom">
+      <p class="percentage-ab-card">
+        91%
+      </p>
+      <p class="d-ab-p">
+        Speed Grade
+      </p>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="pre-text-banner-poster">
+  <h3 class="pre-heading">
+    Largest Library of Pre-Built Websites
+  </h3>
+  <p class="pre-p">
+    Take a look at these gorgeous websites made with Astra and various Page Builders. These are available for import with Astra Sites plugin.
+  </p>
+  <div class="pre-btn-box">
+    <button class="pre-btn">
+      
+      <i class="fa-solid fa-arrow-right"></i> SEE ALL SITES
+    </button>
+  </div>
+</div>
+<div class="empty"></div>
+<footer class="footer-card">
+<div class="footer-container">
+  <div class="footer-main-col-sec1">
+   <div class="footer-col-card">
+    <div class="footer-col-c-i">
+      <i class="fa-brands fa-facebook-f"></i>
+    </div>
+   <div class="col-span-c-span">
+  
+    <h4 class="col-f-c-h">
+      foacebok group
+    </h4>
+    <p class="col-c-f-p">
+user diccusion forms
+    </p>
+
+  </div>
+
+   </div>
+   <div class="footer-col-card">
+    <div class="footer-col-c-i">
+      <i class="fa-regular fa-user"></i>
+    </div>
+   <div class="col-span-c-span">
+  
+    <h4 class="col-f-c-h">
+     elite support
+    </h4>
+    <p class="col-c-f-p">
+have questions or need help?
+    </p>
+
+  </div>
+
+   </div>
+   <div class="footer-col-card">
+    <div class="footer-col-c-i">
+      <i class="fa-regular fa-pen-to-square"></i>
+    </div>
+   <div class="col-span-c-span">
+  
+    <h4 class="col-f-c-h">
+      documentation
+    </h4>
+    <p class="col-c-f-p">
+artical that cover common questions
+    </p>
+
+  </div>
+
+   </div>
+  </div>
+  <div class="footer-main-col-sec2">
+    <div class="row-card">
+      <h3 class="row-card-heading">
+        OUR PRODUCTS
+      </h3>
+      <ul class="row-card-f-ul">
+        <li>Spectra</li>
+        <li>Ultimate Addons for Beaver Builder</li>
+        <li>Ultimate Addons for Elementor</li>
+        <li>WP Portfolio</li>
+        <li>Convert Pro</li>
+      </ul>
+    </div>
+    <div class="row-card">
+      <h3 class="row-card-heading">
+        RESOURCES
+
+      </h3>
+      <ul class="row-card-f-ul">
+        <li>What’s New</li>
+        <li>Recommendations</li>
+        <li>Translation Project</li>
+        <li>Affiliate Program</li>
+        <li>FAQ</li>
+      </ul>
+    </div>
+    <div class="row-card">
+      <h3 class="row-card-heading">
+        TOP FEATURES
+
+      </h3>
+      <ul class="row-card-f-ul">
+        <li>Astra Pro</li>
+        <li>WooCommerce</li>
+        <li>LifterLMS Integration</li>
+        <li>LearnDash Integration</li>
+        <li>Theme For Elementor</li>
+      </ul>
+    </div>
+    <div class="row-card">
+      <h3 class="row-card-heading">
+        HELP & SUPPORT
+
+      </h3>
+      <ul class="row-card-f-ul">
+        <li>Login
+        </li>
+        <li>Downloads</li>
+        <li>Upgrade</li>
+        <li>Video Tutorials</li>
+        <li>Theme For Elementor</li>
+      </ul>
+    </div>
+  </div>
+  <div class="footer-main-col-sec3">
+    <div class="fo-icon-s">
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-twitter"></i>
+
+    </div>
+    <div class="copy-right-clam">
+      Copyright © 2020 -2023 LocalHostCode
+    </div>
+  </div>
+</div>
+</footer>
+
+  </body>
+  <script src="js/index.js"></script>
+</html>
---
+++ astraweb/js/index.js
@@ -0,0 +1,7 @@
+// -------navbar---function-------
+
+let nav=document.querySelector(".right-menu")
+let burger=document.querySelector(".menu-bar")
+burger.onclick=()=>{
+    nav.classList.toggle("nav-toggle")
+}
